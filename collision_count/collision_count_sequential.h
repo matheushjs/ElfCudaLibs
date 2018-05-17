361a4f5c790ed224129d389c4e8b9c620684a78d
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-05-17 19:19:25 -0300

    Fix milisecond to second conversion

--- collision_count/collision_count_sequential.h
+++ collision_count/collision_count_sequential.h
@@ -32,7 +32,7 @@ void test_count(int3 *vector, int size, int iters){
 	for(i = 0; i < iters; i++){
 		count_collisions(vector, size);
 	}
-	printf("Elapsed: %lf ms\n", (clock() - beg) / (double) CLOCKS_PER_SEC / 1000);
+	printf("Elapsed: %lf ms\n", (clock() - beg) / (double) CLOCKS_PER_SEC * 1000);
 }
 
 #endif /* COLLISION_COUNT_SEQUENTIAL_H_ */
