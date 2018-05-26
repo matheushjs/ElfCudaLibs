37b80f5c53c44c112cd211f7226b48d4bbd01988
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-05-26 20:19:52 -0300

    Print collisions afterwards

--- collision_count/c_code/collision_count_sequential.h
+++ collision_count/c_code/collision_count_sequential.h
@@ -7,13 +7,15 @@
 #include "quadratic.h"
 
 void test_count(int3 *vector, int size, int iters){
-	int i;
+	int i, res;
 
 	int beg = clock();
 	for(i = 0; i < iters; i++){
-		int res = count_collisions(vector, size);
+		res = count_collisions(vector, size);
 	}
+
 	printf("Elapsed: %lf ms\n", (clock() - beg) / (double) CLOCKS_PER_SEC * 1000);
+	printf("Collisions: %d\n", res);
 }
 
 #endif /* COLLISION_COUNT_SEQUENTIAL_H_ */
