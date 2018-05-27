31a4f65493b84ca4f08f7fac9f856040f9466836
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-05-27 17:46:05 -0300

    Fix stupid bug with integer overflow

--- collision_count/collision_count_sequential_linear.cuh
+++ collision_count/collision_count_sequential_linear.cuh
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 #define COORD3D(V, AXIS) COORD(V.x, V.y, V.z, AXIS)
-#define COORD(X, Y, Z, AXIS) ( (Z+AXIS/2) * (AXIS*AXIS) + (Y+AXIS/2) * (AXIS) + (X+AXIS/2))
+#define COORD(X, Y, Z, AXIS) ( (Z+AXIS/2) * ((long int) AXIS*AXIS) + (Y+AXIS/2) * ((long int) AXIS) + (X+AXIS/2))
 
 #define AXISTYPE char
 
@@ -16,21 +16,20 @@
  * 'space3d' is expected to be a vector with size:
  *    { [ (size + 1) * 2 ] ** 3 } * sizeof(AXISTYPE)
  */
-int count_collisions(int3 *vector, int size, AXISTYPE *space3d){
+int count_collisions(int3 *vector, int size, AXISTYPE *space3d, int axisSize){
 	int i, collisions;
-	int axisSize = (size + 1) * 2;
 
 	collisions = 0;
 
 	// Reset space
 	for(i = 0; i < size; i++){
-		int idx = COORD3D(vector[i], axisSize);
+		long int idx = COORD3D(vector[i], axisSize);
 		space3d[idx] = 0;
 	}
 
 	// Place beads in the space (actually calculate the collisions at the same time)
 	for(i = 0; i < size; i++){
-		int idx = COORD3D(vector[i], axisSize);
+		long int idx = COORD3D(vector[i], axisSize);
 		if(space3d[idx]){
 			collisions += space3d[idx];
 		}
@@ -46,11 +45,11 @@ void test_count(int3 *vector, int size, int iters){
 	int beg = clock();
 	
 	int axisSize = (size + 1) * 2;
-	unsigned long memSize = sizeof(AXISTYPE) * axisSize * axisSize * axisSize;
+	unsigned long memSize = sizeof(AXISTYPE) * axisSize * axisSize * (unsigned long) axisSize;
 	AXISTYPE *space3d = (AXISTYPE *) malloc(memSize);
 	printf("Memsize: %lf Gb, Pointer: %p\n", memSize / (double) 1024 / 1024 / 1024, space3d);
 	for(i = 0; i < iters; i++){
-		int res = count_collisions(vector, size, space3d);
+		int res = count_collisions(vector, size, space3d, axisSize);
 	}
 
 	free(space3d);
