e7983eef50828b68999b8f4b706a68194b72b2f7
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-07-23 17:10:10 -0300

    Removed volatile var that fixed old problem

--- ElfColCnt/Sequential_Quadratic/test.cuh
+++ ElfColCnt/Sequential_Quadratic/test.cuh
@@ -10,9 +10,6 @@ int test_count(int3 *vector, int size, int iters){
 	int beg = clock();
 	for(i = 0; i < iters; i++){
 		res = count_collisions(vector, size);
-
-		volatile int a;
-		a = res;
 	}
 
 	printf("Elapsed: %lf ms\n", (clock() - beg) / (double) CLOCKS_PER_SEC * 1000);
