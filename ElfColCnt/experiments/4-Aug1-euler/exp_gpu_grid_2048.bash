c92540a233fa0e381b9e05ee4cf95deb29dfc189
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-08-01 10:32:25 -0300

    Add experiment

---
+++ ElfColCnt/experiments/4-Aug1-euler/exp_gpu_grid_2048.bash
@@ -0,0 +1,47 @@
+#PBS -N ColCntGpuBig
+#PBS -l select=1:ngpus=1
+#PBS -l walltime=24:00:00
+#PBS -m abe
+#PBS -M matheus.saldanha@usp.br
+
+# load modules
+module load gcc
+module load cuda-toolkit/9.0.176
+
+DIR=/home/mathjs/collision_count
+cd $DIR
+
+# Here we will experiment with the multiple-blocks approaches
+# We want to plot a graph [problem size]x[execution time]
+
+
+binaries="ns_sr hs_sr ns_mr ss_at ss_ht"
+
+for bin in $binaries; do
+	make $bin;
+done;
+
+time {
+	for progName in $binaries; do
+		echo "id,psize,elapsed,real,user,system" > $progName.out
+
+		psizeBegin=128
+		psizeInc=$((256))
+		psizeEnd=$((4096+128))
+
+		for problemSize in $(seq $psizeBegin $psizeInc $psizeEnd); do
+			for executionId in $(seq 10); do
+				# Echo progress onto stderr
+				echo $progName $problemSize $executionId 1>&2
+
+				# Run code
+				output=$( { time ./$progName $problemSize 60000 | grep Elapsed; } 2>&1 )
+				
+				# Ouput as csv
+				echo -n ${executionId},
+				echo -n ${problemSize},
+				echo $output | cut -f2,5,7,9 -d' ' | sed -e "s/ /,/g"
+			done;
+		done >> $progName.out;
+	done;
+}
