df3c6147396ca018cabcaf32c4b213cc9232c1e2
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-10-20 19:29:00 -0300

    Add option to run in batch mode in the gpu

--- ElfColCnt/cuda_nsteps/test.h
+++ ElfColCnt/cuda_nsteps/test.h
@@ -4,6 +4,12 @@
 
 #include "ElfColCnt.h"
 
+#ifdef BATCH
+	#define BATCH 1
+#else
+	#define BATCH 0
+#endif
+
 int test_count(ElfFloat3d *vector, int size, int iters){
 	struct CollisionCountPromise *promises;
 	promises = (struct CollisionCountPromise *) malloc(sizeof(struct CollisionCountPromise) * iters);
@@ -11,16 +17,22 @@ int test_count(ElfFloat3d *vector, int size, int iters){
 	int beg = clock();
 
 	int i, res;
+#if BATCH == 1
 	for(i = 0; i < iters; i++){
 		promises[i] = count_collisions_launch(vector, size);
 	}
-
 	for(i = 0; i < iters; i++){
 		res = count_collisions_fetch(promises[i]);
 	}
+#else
+	for(i = 0; i < iters; i++){
+		promises[i] = count_collisions_launch(vector, size);
+		res = count_collisions_fetch(promises[i]);
+	}
+#endif
 
 	printf("Elapsed: %lf ms\n", (clock() - beg) / (double) CLOCKS_PER_SEC * 1000);
-	printf("Collisions [N Steps Grid 1Row]: %d\n", res);
+	printf("Collisions [N Steps Grid 1Row %s]: %d\n", BATCH ? "Batch" : "Non-Batch", res);
 
 	return res;
 }
