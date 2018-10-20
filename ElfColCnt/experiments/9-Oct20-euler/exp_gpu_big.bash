71bc974cd5392b904752fa9c29fb68bda7d1c26c
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-10-20 20:41:40 -0300

    Add experiments

---
+++ ElfColCnt/experiments/9-Oct20-euler/exp_gpu_big.bash
@@ -0,0 +1,44 @@
+#PBS -N ColCntGpuBig
+#PBS -l select=1:ngpus=1
+#PBS -l walltime=24:00:00
+#PBS -m abe
+#PBS -M matheus.saldanha@usp.br
+
+# load modules
+module load gcc;
+module load cuda-toolkit/9.0.176;
+
+DIR=/home/mathjs/ElfColCnt;
+cd $DIR;
+
+binaries="cuda_n cuda_half";
+
+for bin in $binaries; do
+	make $bin;
+done;
+
+for progName in $binaries; do
+	echo "psize,real,user,system" > $progName.out;
+
+	beg=1024;
+	inc=65536;
+	end=$((beg + 16*inc));
+	iterations=100;
+
+	echo "Begins at $beg and ends at $end" 1>&2;
+
+	for problemSize in $(seq $beg $inc $end); do
+		# Do some warmup runs
+		./$progName $problemSize 5 2>&1 > /dev/null;
+
+		# Echo progress onto stderr
+		echo $progName $problemSize $executionId 1>&2;
+
+		# Run code
+		output=$( { /usr/bin/time -p ./$progName $problemSize $iterations > /dev/null; } 2>&1 );
+		
+		# Ouput as csv
+		echo -n ${problemSize},;
+		echo $output | cut -f2,4,6 -d' ' | sed -e "s/ /,/g";
+	done >> $progName.out;
+done;
---
+++ ElfColCnt/experiments/9-Oct20-euler/exp_seq_quad.bash
@@ -0,0 +1,41 @@
+#PBS -N ColCntGpuBig
+#PBS -l select=1:ngpus=1
+#PBS -l walltime=24:00:00
+#PBS -m abe
+#PBS -M matheus.saldanha@usp.br
+
+# load modules
+module load gcc;
+
+DIR=/home/mathjs/ElfColCnt;
+cd $DIR;
+
+binaries="seq_quad seq_lin";
+
+make $binaries;
+
+for progName in $binaries; do
+	echo "psize,elapsed" > $progName.out;
+
+	beg=64;
+	inc=64;
+	end=$((1024*8));
+	iterations=100;
+
+	echo "Begins at $beg and ends at $end" 1>&2;
+
+	for problemSize in $(seq $beg $inc $end); do
+		# Do some warmup runs
+		./$progName $problemSize 5 &> /dev/null;
+
+		# Echo progress onto stderr
+		echo $profName $problemSize $executionId 1>&2;
+
+		# Run code
+		output=$( ./$progName $problemSize $iterations | grep Elapsed );
+
+		# Ouput as csv
+		echo -n ${problemSize},;
+		echo $output | cut -f2 -d' ';
+	done >> $progName.out;
+done;
