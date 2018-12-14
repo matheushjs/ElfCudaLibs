43fe167e3a3d6077244ce90de4c2a20f1f8ec2e7
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-12-14 02:30:49 -0200

    Clean exec.

--- ElfColCnt/src/exec.c
+++ ElfColCnt/src/exec.c
@@ -4,182 +4,223 @@
 #include <limits.h>
 
 /*
- * MACROS for building programs BUILD_TEST mode or 'run' mode
- *    TEST    - when run, the program will execute the batch of tests we defined
- *    nothing - when run, the program will count collisions for a vector of size provided as program argument
- * If nothing is specified, 'run' is assumed.
- */
-#ifdef BUILD_TEST
-	#define BUILD_TEST 1
-#endif
-
-/*
  * MACROS for defining which collision count method to test.
  */
 #ifdef SEQ_QUAD
 	#define SEQ_QUAD 1
-	#include "Sequential_Quadratic_test.h"
+	#include "Sequential_Quadratic.h"
 #endif
 #ifdef SEQ_LIN
 	#define SEQ_LIN 1
-	#include "Sequential_Linear_test.h"
+	#include "Sequential_Linear.h"
 #endif
-#ifdef CUDA_HALF
-	#define CUDA_HALF 1
-	#include "CUDA_Proposed_Alg_test.h"
+#ifdef CUDA_PROPOSED
+	#define CUDA_PROPOSED 1
+	#include "CUDA_Proposed_Alg.h"
 #endif
-#ifdef CUDA_N
-	#define CUDA_N 1
-	#include "CUDA_Usual_Alg_test.h"
+#ifdef CUDA_USUAL
+	#define CUDA_USUAL 1
+	#include "CUDA_Usual_Alg.h"
 #endif
-#if SEQ_QUAD != 1 && SEQ_LIN != 1 && CUDA_HALF != 1 && CUDA_N != 1
-	#error "Must specify method!"
+#if SEQ_QUAD != 1 && SEQ_LIN != 1 && CUDA_PROPOSED != 1 && CUDA_USUAL != 1
+	#error "Must specify method using \"-D [method]\". [method] can be SEQ_QUAD SEQ_LIN CUDA_PROPOSED or CUDA_USUAL"
 #endif
 
 // Utilities for creating vectors and tests
 #include "utils.h"
 
-void run(int vecSize, int iters, double std, int rrate){
-	// int vecSize = 1000;
-	// int iters = 10000;
-	int i;
 
+#ifdef SEQ_LIN
+void run(int vecSize, int iters, double std, int rrate){
+	int i;
 	srand(72);
 
-	// For the sequential versions, we take care to to abuse of cache.
-	#if SEQ_LIN == 1
-		int beg = clock();
-		int res = 0;
+	int beg = clock();
+	int res = 0;
+	static AXISTYPE *space3d = NULL;
+	int axisSize = (vecSize + 1) * 2;
+	unsigned long memSize = sizeof(AXISTYPE) * axisSize * axisSize * (unsigned long) axisSize;
 
-		for(i = 0; i < iters; i++){
-			if(i%rrate == 0){
-				// Force reallocation of the space3d array
-				test_count(NULL, -1, -1);
-			}
-			// ElfInt3d *vec = vector_protein(vecSize);   // Create a new random vector
-			ElfInt3d *vec = vector_rnorm(vecSize, vecSize, std);   // Create a new random vector
-			res = test_count(vec, vecSize, 1);     // Count collisions for it
-			free(vec);                                    // Free vector
+	// Perform many iterations to collect the time spent
+	for(i = 0; i < iters; i++){
+		// Reallocate space array
+		if(i%rrate == 0){
+			free(space3d);
+			space3d = (AXISTYPE *) malloc(memSize);
 		}
-		test_count(NULL, -1, -1);                  // Sinalize to the test_count() to free inner global resources
-		printf("Elapsed: %lf ms\n", (clock() - beg) / (double) CLOCKS_PER_SEC * 1000);
-		printf("Collisions [Linear]: %d\n", res);
-	#elif SEQ_QUAD == 1
-		int beg = clock();
-		int res = 0;
-		for(i = 0; i < iters; i++){
-			// ElfInt3d *vec = vector_protein(vecSize);   // Create a new random vector
-			ElfInt3d *vec = vector_rnorm(vecSize, vecSize, std);   // Create a new random vector
-			res = test_count(vec, vecSize, 1);     // Count collisions for it
-			free(vec);                             // Free vector
-		}
-		printf("Elapsed: %lf ms\n", (clock() - beg) / (double) CLOCKS_PER_SEC * 1000);
-		printf("Collisions [Quadratic]: %d\n", res);
-	#else
-		ElfFloat3d *vec = vector_rand_f(vecSize);
-		test_count(vec, vecSize, iters);
-		free(vec);
-	#endif
+
+		// Create a new random vector
+		// ElfInt3d *vec = vector_protein(vecSize);
+		ElfInt3d *vec = vector_rnorm(vecSize, vecSize, std);
+		res = count_collisions(vec, vecSize, space3d, axisSize);
+		free(vec); // Free vector
+	}
+	free(space3d);
+
+	printf("CPU time: %lf ms\n", (clock() - beg) / (double) CLOCKS_PER_SEC * 1000);
+	printf("Collisions [Linear]: %d\n", res);
 }
-
-void test(){
-	int size = 16 * 1024;
-	int gold, res;
-
-	// SEQ_LIN cannot handle big vectors
-	#if SEQ_LIN == 1
-		goto small;
-	#endif
-
-	// Allocate the vectors we need
-	#if SEQ_QUAD == 1 || SEQ_LIN == 1
-		ElfInt3d *seq             = vector_seq(size);
-		ElfInt3d *neigh_paircolls = vector_neigh_paircolls(size);
-		ElfInt3d *rand_paircolls  = vector_rand_paircolls(size);
-	#else
-		ElfFloat3d *seq             = vector_seq_f(size);
-		ElfFloat3d *neigh_paircolls = vector_neigh_paircolls_f(size);
-		ElfFloat3d *rand_paircolls  = vector_rand_paircolls_f(size);
-	#endif
-
-	gold = 0;
-	res = test_count(seq, size, 1);
-	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
-
-	gold = size / 2;
-	res = test_count(neigh_paircolls, size, 1);
-	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
-
-	gold = size / 2;
-	res = test_count(rand_paircolls, size, 1);
-	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
-
-	free(seq);
-	free(neigh_paircolls);
-	free(rand_paircolls);
-
-	// Then we repeat the above, with vectors of more irregular size
-	size = 16 * 1024 + 220;
-
-	#if SEQ_QUAD == 1 || SEQ_LIN == 1
-		seq             = vector_seq(size);
-		neigh_paircolls = vector_neigh_paircolls(size);
-		rand_paircolls  = vector_rand_paircolls(size);
-	#else
-		seq             = vector_seq_f(size);
-		neigh_paircolls = vector_neigh_paircolls_f(size);
-		rand_paircolls  = vector_rand_paircolls_f(size);
-	#endif
-
-	gold = 0;
-	res = test_count(seq, size, 1);
-	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
-
-	gold = size / 2;
-	res = test_count(neigh_paircolls, size, 1);
-	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
-
-	gold = size / 2;
-	res = test_count(rand_paircolls, size, 1);
-	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
-
-	free(seq);
-	free(neigh_paircolls);
-	free(rand_paircolls);
-
-#if SEQ_LIN == 1
-small:
 #endif
 
-	// Then we repeat the above, with a small vector
-	size = 220;
+#ifdef SEQ_QUAD
+void run(int vecSize, int iters, double std, int rrate){
+	int i;
+	srand(72);
 
-	#if SEQ_QUAD == 1 || SEQ_LIN == 1
-		seq             = vector_seq(size);
-		neigh_paircolls = vector_neigh_paircolls(size);
-		rand_paircolls  = vector_rand_paircolls(size);
-	#else
-		seq             = vector_seq_f(size);
-		neigh_paircolls = vector_neigh_paircolls_f(size);
-		rand_paircolls  = vector_rand_paircolls_f(size);
-	#endif
+	int beg = clock();
+	int res = 0;
 
-	gold = 0;
-	res = test_count(seq, size, 1);
-	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
+	for(i = 0; i < iters; i++){
+		// Create a new random vector
+		// ElfInt3d *vec = vector_protein(vecSize);
+		ElfInt3d *vec = vector_rnorm(vecSize, vecSize, std);
+		res = count_collisions(vec, vecSize);
+		free(vec); // Free vector
+	}
 
-	gold = size / 2;
-	res = test_count(neigh_paircolls, size, 1);
-	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
-
-	gold = size / 2;
-	res = test_count(rand_paircolls, size, 1);
-	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
-
-	free(seq);
-	free(neigh_paircolls);
-	free(rand_paircolls);
+	printf("CPU Time: %lf ms\n", (clock() - beg) / (double) CLOCKS_PER_SEC * 1000);
+	printf("Collisions [Quadratic]: %d\n", res);
 }
+#endif
+
+#ifdef CUDA_PROPOSED
+void run(int vecSize, int iters, double std, int rrate){
+	int i, res;
+	struct CollisionCountPromise promise;
+
+	ElfFloat3d *vec = vector_rand_f(vecSize);
+
+	int beg = clock();
+	for(i = 0; i < iters; i++){
+		promise = count_collisions_launch(vec, vecSize);
+		res = count_collisions_fetch(promise);
+	}
+
+	printf("CPU Time: %lf ms\n", (clock() - beg) / (double) CLOCKS_PER_SEC * 1000);
+	printf("Collisions [CUDA Proposed]: %d\n", res);
+
+	free(vec);
+}
+#endif
+
+#ifdef CUDA_USUAL
+void run(int vecSize, int iters, double std, int rrate){
+	int i, res;
+	struct CollisionCountPromise promise;
+
+	ElfFloat3d *vec = vector_rand_f(vecSize);
+
+	int beg = clock();
+	for(i = 0; i < iters; i++){
+		promise = count_collisions_launch(vec, vecSize);
+		res = count_collisions_fetch(promise);
+	}
+
+	printf("CPU Time: %lf ms\n", (clock() - beg) / (double) CLOCKS_PER_SEC * 1000);
+	printf("Collisions [CUDA Usual]: %d\n", res);
+
+	free(vec);
+}
+#endif
+
+//void test(){
+//	int size = 16 * 1024;
+//	int gold, res;
+//
+//	// SEQ_LIN cannot handle big vectors
+//	#if SEQ_LIN == 1
+//		goto small;
+//	#endif
+//
+//	// Allocate the vectors we need
+//	#if SEQ_QUAD == 1 || SEQ_LIN == 1
+//		ElfInt3d *seq             = vector_seq(size);
+//		ElfInt3d *neigh_paircolls = vector_neigh_paircolls(size);
+//		ElfInt3d *rand_paircolls  = vector_rand_paircolls(size);
+//	#else
+//		ElfFloat3d *seq             = vector_seq_f(size);
+//		ElfFloat3d *neigh_paircolls = vector_neigh_paircolls_f(size);
+//		ElfFloat3d *rand_paircolls  = vector_rand_paircolls_f(size);
+//	#endif
+//
+//	gold = 0;
+//	res = test_count(seq, size, 1);
+//	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
+//
+//	gold = size / 2;
+//	res = test_count(neigh_paircolls, size, 1);
+//	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
+//
+//	gold = size / 2;
+//	res = test_count(rand_paircolls, size, 1);
+//	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
+//
+//	free(seq);
+//	free(neigh_paircolls);
+//	free(rand_paircolls);
+//
+//	// Then we repeat the above, with vectors of more irregular size
+//	size = 16 * 1024 + 220;
+//
+//	#if SEQ_QUAD == 1 || SEQ_LIN == 1
+//		seq             = vector_seq(size);
+//		neigh_paircolls = vector_neigh_paircolls(size);
+//		rand_paircolls  = vector_rand_paircolls(size);
+//	#else
+//		seq             = vector_seq_f(size);
+//		neigh_paircolls = vector_neigh_paircolls_f(size);
+//		rand_paircolls  = vector_rand_paircolls_f(size);
+//	#endif
+//
+//	gold = 0;
+//	res = test_count(seq, size, 1);
+//	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
+//
+//	gold = size / 2;
+//	res = test_count(neigh_paircolls, size, 1);
+//	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
+//
+//	gold = size / 2;
+//	res = test_count(rand_paircolls, size, 1);
+//	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
+//
+//	free(seq);
+//	free(neigh_paircolls);
+//	free(rand_paircolls);
+//
+//#if SEQ_LIN == 1
+//small:
+//#endif
+//
+//	// Then we repeat the above, with a small vector
+//	size = 220;
+//
+//	#if SEQ_QUAD == 1 || SEQ_LIN == 1
+//		seq             = vector_seq(size);
+//		neigh_paircolls = vector_neigh_paircolls(size);
+//		rand_paircolls  = vector_rand_paircolls(size);
+//	#else
+//		seq             = vector_seq_f(size);
+//		neigh_paircolls = vector_neigh_paircolls_f(size);
+//		rand_paircolls  = vector_rand_paircolls_f(size);
+//	#endif
+//
+//	gold = 0;
+//	res = test_count(seq, size, 1);
+//	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
+//
+//	gold = size / 2;
+//	res = test_count(neigh_paircolls, size, 1);
+//	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
+//
+//	gold = size / 2;
+//	res = test_count(rand_paircolls, size, 1);
+//	printf("Size %d: %s\n", size, gold == res ? "SUCCESS!" : "FAILURE");
+//
+//	free(seq);
+//	free(neigh_paircolls);
+//	free(rand_paircolls);
+//}
 
 
 int main(int argc, char *argv[]){
@@ -210,7 +251,7 @@ int main(int argc, char *argv[]){
 			rrate   = atoi(argv[4]);
 			break;
 		default:
-			fprintf(stderr, "Usage: %s [problem_size] [no. iterations] [std deviation]i [realloc_rate]\n", argv[0]);
+			fprintf(stderr, "Usage: %s [problem_size] [no. iterations] [std deviation] [realloc_rate]\n", argv[0]);
 			return 1;
 	}
 	
